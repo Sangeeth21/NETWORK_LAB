4bd08be764a9419ed11cbe6d66ca0d511d68cb4e
SANGEETH SUBHASH <84234657+Sangeeth21@users.noreply.github.com>
2022-05-25 09:54:42 +0530

    Update TCP_CLIENT.c

--- TCP_CLIENT.c
+++ TCP_CLIENT.c
@@ -10,15 +10,15 @@ void func(int sockfd)
 {
 	char buff[MAX];
 	int n;
-	for (;;) {
-		bzero(buff, sizeof(buff));
-		printf("Enter the string : ");
+	for (;;) {       //infinte for loop
+		bzero(buff, sizeof(buff));       //temporary angane thanne irikatte
+		printf("Enter the string : ");     
 		n = 0;
 		while ((buff[n++] = getchar()) != '\n')
 			;
 		write(sockfd, buff, sizeof(buff));
 		bzero(buff, sizeof(buff));
-		read(sockfd, buff, sizeof(buff));
+		read(sockfd, buff, sizeof(buff));  
 		printf("From Server : %s", buff);
 		if ((strncmp(buff, "exit", 4)) == 0) {
 			printf("Client Exit...\n");
